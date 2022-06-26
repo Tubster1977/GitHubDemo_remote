fedb597574b18197e8c2c58d4f05041c738b0077
Tubster1977 <troy9j77@googlemail.com>
2022-06-26 14:29:36 +0100

    Changed text content

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("This is a test!")
             .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
