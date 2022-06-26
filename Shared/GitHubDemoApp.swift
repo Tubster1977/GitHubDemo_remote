390c9bfcfbab20dcaf4637751804ffdabe194a36
Tubster1977 <troy9j77@googlemail.com>
2022-06-26 14:25:47 +0100

    Initial Commit

---
+++ Shared/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  Shared
+//
+//  Created by Admin on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
